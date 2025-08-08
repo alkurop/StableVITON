8189dfe3ac3d4cf28f9bcbfd4baad2f579f5888b
Alex <alkurop@gmail.com>
2025-08-08 21:37:24 +0200

    Ignore hint

--- ldm/modules/attention.py
+++ ldm/modules/attention.py
@@ -181,7 +181,9 @@ class CrossAttention(nn.Module):
         )
         
 
-    def forward(self, x, context=None, mask=None, mask1=None, mask2=None, use_attention_tv_loss=False):
+    def forward(self, x, context=None, mask=None, mask1=None, mask2=None, use_attention_tv_loss=False,  hint=None,):
+        # We ignore `hint` unless you want to use it later
+
         h = self.heads
         is_self_attn = context is None
         q = self.to_q(x)
